e56bb1712c14c3df1b58ec410fa1291188bf96ab
Vesturcikas <vestas1976@gmail.com>
2018-12-16 17:22:42 +0200

    Suformuota Komplektu Lentele K5 duomenu bazeje

--- K5.v3/Form3.Designer.cs
+++ K5.v3/Form3.Designer.cs
@@ -29,15 +29,18 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.perziureti_KomplektuDetaliuLentele = new System.Windows.Forms.Button();
-            this.uzdaryti_forma3 = new System.Windows.Forms.Button();
-            this.perziureti_KomplektuLentele = new System.Windows.Forms.Button();
             this.perrasyti_KomplektuDetaliuLentele = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.perziureti_KomplektuDetaliuLentele = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ieskoti_KomplektuCsvFailo = new System.Windows.Forms.Button();
+            this.perrasyti_KomplektuLenteleK5DB = new System.Windows.Forms.Button();
+            this.perziureti_KomplektuLentele = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.uzdaryti_forma3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -54,9 +57,31 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "K5. Komplektu detaliu lentelė";
             // 
+            // perrasyti_KomplektuDetaliuLentele
+            // 
+            this.perrasyti_KomplektuDetaliuLentele.Location = new System.Drawing.Point(570, 95);
+            this.perrasyti_KomplektuDetaliuLentele.Name = "perrasyti_KomplektuDetaliuLentele";
+            this.perrasyti_KomplektuDetaliuLentele.Size = new System.Drawing.Size(186, 37);
+            this.perrasyti_KomplektuDetaliuLentele.TabIndex = 1;
+            this.perrasyti_KomplektuDetaliuLentele.Text = "Perrašyti";
+            this.perrasyti_KomplektuDetaliuLentele.UseVisualStyleBackColor = true;
+            // 
+            // perziureti_KomplektuDetaliuLentele
+            // 
+            this.perziureti_KomplektuDetaliuLentele.Location = new System.Drawing.Point(377, 95);
+            this.perziureti_KomplektuDetaliuLentele.Name = "perziureti_KomplektuDetaliuLentele";
+            this.perziureti_KomplektuDetaliuLentele.Size = new System.Drawing.Size(186, 37);
+            this.perziureti_KomplektuDetaliuLentele.TabIndex = 0;
+            this.perziureti_KomplektuDetaliuLentele.Text = "Peržiūrėti";
+            this.perziureti_KomplektuDetaliuLentele.UseVisualStyleBackColor = true;
+            this.perziureti_KomplektuDetaliuLentele.Click += new System.EventHandler(this.perziureti_KomplektuDetaliuLentele_Click);
+            // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.ieskoti_KomplektuCsvFailo);
+            this.groupBox2.Controls.Add(this.perrasyti_KomplektuLenteleK5DB);
             this.groupBox2.Controls.Add(this.perziureti_KomplektuLentele);
             this.groupBox2.Location = new System.Drawing.Point(24, 184);
             this.groupBox2.Name = "groupBox2";
@@ -64,6 +89,54 @@
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "K5. Komplektu lentelė";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Komplektų sąrašo .csv failas:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(546, 37);
+            this.textBox1.TabIndex = 4;
+            // 
+            // ieskoti_KomplektuCsvFailo
+            // 
+            this.ieskoti_KomplektuCsvFailo.Location = new System.Drawing.Point(568, 35);
+            this.ieskoti_KomplektuCsvFailo.Name = "ieskoti_KomplektuCsvFailo";
+            this.ieskoti_KomplektuCsvFailo.Size = new System.Drawing.Size(186, 37);
+            this.ieskoti_KomplektuCsvFailo.TabIndex = 3;
+            this.ieskoti_KomplektuCsvFailo.Text = "Ieškoti";
+            this.ieskoti_KomplektuCsvFailo.UseVisualStyleBackColor = true;
+            this.ieskoti_KomplektuCsvFailo.Click += new System.EventHandler(this.ieskoti_KomplektuCsvFailo_Click);
+            // 
+            // perrasyti_KomplektuLenteleK5DB
+            // 
+            this.perrasyti_KomplektuLenteleK5DB.Location = new System.Drawing.Point(570, 84);
+            this.perrasyti_KomplektuLenteleK5DB.Name = "perrasyti_KomplektuLenteleK5DB";
+            this.perrasyti_KomplektuLenteleK5DB.Size = new System.Drawing.Size(186, 37);
+            this.perrasyti_KomplektuLenteleK5DB.TabIndex = 2;
+            this.perrasyti_KomplektuLenteleK5DB.Text = "Perrašyti";
+            this.perrasyti_KomplektuLenteleK5DB.UseVisualStyleBackColor = true;
+            this.perrasyti_KomplektuLenteleK5DB.Click += new System.EventHandler(this.perrasyti_KomplektuLenteleK5DB_Click);
+            // 
+            // perziureti_KomplektuLentele
+            // 
+            this.perziureti_KomplektuLentele.Location = new System.Drawing.Point(377, 84);
+            this.perziureti_KomplektuLentele.Name = "perziureti_KomplektuLentele";
+            this.perziureti_KomplektuLentele.Size = new System.Drawing.Size(186, 37);
+            this.perziureti_KomplektuLentele.TabIndex = 1;
+            this.perziureti_KomplektuLentele.Text = "Peržiūrėti";
+            this.perziureti_KomplektuLentele.UseVisualStyleBackColor = true;
+            this.perziureti_KomplektuLentele.Click += new System.EventHandler(this.perziureti_KomplektuLentele_Click);
             // 
             // groupBox3
             // 
@@ -76,53 +149,14 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "K5. Parduotų detalių lentelė";
             // 
-            // perziureti_KomplektuDetaliuLentele
+            // button3
             // 
-            this.perziureti_KomplektuDetaliuLentele.Location = new System.Drawing.Point(377, 95);
-            this.perziureti_KomplektuDetaliuLentele.Name = "perziureti_KomplektuDetaliuLentele";
-            this.perziureti_KomplektuDetaliuLentele.Size = new System.Drawing.Size(186, 37);
-            this.perziureti_KomplektuDetaliuLentele.TabIndex = 0;
-            this.perziureti_KomplektuDetaliuLentele.Text = "Peržiūrėti";
-            this.perziureti_KomplektuDetaliuLentele.UseVisualStyleBackColor = true;
-            this.perziureti_KomplektuDetaliuLentele.Click += new System.EventHandler(this.perziureti_KomplektuDetaliuLentele_Click);
-            // 
-            // uzdaryti_forma3
-            // 
-            this.uzdaryti_forma3.Location = new System.Drawing.Point(594, 496);
-            this.uzdaryti_forma3.Name = "uzdaryti_forma3";
-            this.uzdaryti_forma3.Size = new System.Drawing.Size(185, 37);
-            this.uzdaryti_forma3.TabIndex = 3;
-            this.uzdaryti_forma3.Text = "Baigti";
-            this.uzdaryti_forma3.UseVisualStyleBackColor = true;
-            this.uzdaryti_forma3.Click += new System.EventHandler(this.uzdaryti_forma3_Click);
-            // 
-            // perziureti_KomplektuLentele
-            // 
-            this.perziureti_KomplektuLentele.Location = new System.Drawing.Point(377, 84);
-            this.perziureti_KomplektuLentele.Name = "perziureti_KomplektuLentele";
-            this.perziureti_KomplektuLentele.Size = new System.Drawing.Size(186, 37);
-            this.perziureti_KomplektuLentele.TabIndex = 1;
-            this.perziureti_KomplektuLentele.Text = "Peržiūrėti";
-            this.perziureti_KomplektuLentele.UseVisualStyleBackColor = true;
-            this.perziureti_KomplektuLentele.Click += new System.EventHandler(this.perziureti_KomplektuLentele_Click);
-            // 
-            // perrasyti_KomplektuDetaliuLentele
-            // 
-            this.perrasyti_KomplektuDetaliuLentele.Location = new System.Drawing.Point(570, 95);
-            this.perrasyti_KomplektuDetaliuLentele.Name = "perrasyti_KomplektuDetaliuLentele";
-            this.perrasyti_KomplektuDetaliuLentele.Size = new System.Drawing.Size(186, 37);
-            this.perrasyti_KomplektuDetaliuLentele.TabIndex = 1;
-            this.perrasyti_KomplektuDetaliuLentele.Text = "Perrašyti";
-            this.perrasyti_KomplektuDetaliuLentele.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(570, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(186, 37);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Perrašyti";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(570, 84);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Papildyti lentelę";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
@@ -133,14 +167,15 @@
             this.button2.Text = "Peržiūrėti";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // uzdaryti_forma3
             // 
-            this.button3.Location = new System.Drawing.Point(570, 84);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(186, 37);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Papildyti lentelę";
-            this.button3.UseVisualStyleBackColor = true;
+            this.uzdaryti_forma3.Location = new System.Drawing.Point(594, 496);
+            this.uzdaryti_forma3.Name = "uzdaryti_forma3";
+            this.uzdaryti_forma3.Size = new System.Drawing.Size(185, 37);
+            this.uzdaryti_forma3.TabIndex = 3;
+            this.uzdaryti_forma3.Text = "Baigti";
+            this.uzdaryti_forma3.UseVisualStyleBackColor = true;
+            this.uzdaryti_forma3.Click += new System.EventHandler(this.uzdaryti_forma3_Click);
             // 
             // Form3
             // 
@@ -155,6 +190,7 @@
             this.Text = "K5 duomenu bazes peržiūra";
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -169,8 +205,11 @@
         private System.Windows.Forms.Button uzdaryti_forma3;
         private System.Windows.Forms.Button perziureti_KomplektuLentele;
         private System.Windows.Forms.Button perrasyti_KomplektuDetaliuLentele;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button perrasyti_KomplektuLenteleK5DB;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ieskoti_KomplektuCsvFailo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- K5.v3/Form3.cs
+++ K5.v3/Form3.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace K5.v3
 {
@@ -35,6 +36,90 @@ namespace K5.v3
             Form4 forma4 = new Form4();
             this.Close();
             forma4.Show();
+        }
+
+        private void ieskoti_KomplektuCsvFailo_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            using (ofd)
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+
+            textBox1.Text = ofd.FileName;
+        }
+
+        private void perrasyti_KomplektuLenteleK5DB_Click(object sender, EventArgs e)
+        {
+            string prisijungimoEilute = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\vesta\source\repos\K5.V3\K5.v3\K5_DB.mdf;Integrated Security=True";
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(textBox1.Text);
+
+            string irasymas = "INSERT INTO Komplektai(Id, KomplektoKodas, Pavadinimas, Det1Kodas, Det1Kiekis, Det2Kodas, Det2Kiekis, Det3Kodas, Det3Kiekis," +
+                "Det4Kodas, Det4Kiekis, Det5Kodas, Det5Kiekis, Det6Kodas, Det6Kiekis,Det7Kodas, Det7Kiekis, Det8Kodas, Det8Kiekis, Det9Kodas, Det9Kiekis) " +
+                "VALUES(@Id, @KomplektoKodas, @Pavadinimas, @Det1Kodas, @Det1Kiekis, @Det2Kodas, @Det2Kiekis, @Det3Kodas, @Det3Kiekis," +
+                "@Det4Kodas, @Det4Kiekis, @Det5Kodas, @Det5Kiekis, @Det6Kodas, @Det6Kiekis, @Det7Kodas, @Det7Kiekis, @Det8Kodas, @Det8Kiekis, @Det9Kodas, @Det9Kiekis)";
+
+            string trynimas = "DELETE FROM Komplektai";
+
+            string line = null;
+
+            SqlConnection sqlPrisijungimas = new SqlConnection(prisijungimoEilute);
+
+            SqlCommand komanadaTrinti = new SqlCommand(trynimas, sqlPrisijungimas);
+
+            using (komanadaTrinti)
+            {
+                sqlPrisijungimas.Open();
+                komanadaTrinti.ExecuteNonQuery();
+                sqlPrisijungimas.Close();
+            }
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                var data = line.Split(';');
+
+                SqlCommand komandaIrasymas = new SqlCommand(irasymas, sqlPrisijungimas);
+
+                using (komandaIrasymas)
+                {
+                    komandaIrasymas.Parameters.AddWithValue("@Id", data[0]);
+                    komandaIrasymas.Parameters.AddWithValue("@KomplektoKodas", data[1]);
+                    komandaIrasymas.Parameters.AddWithValue("@Pavadinimas", data[2]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det1Kodas", data[3]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det1Kiekis", data[4]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det2Kodas", data[5]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det2Kiekis", data[6]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det3Kodas", data[7]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det3Kiekis", data[8]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det4Kodas", data[9]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det4Kiekis", data[10]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det5Kodas", data[11]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det5Kiekis", data[12]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det6Kodas", data[13]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det6Kiekis", data[14]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det7Kodas", data[15]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det7Kiekis", data[16]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det8Kodas", data[17]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det8Kiekis", data[18]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det9Kodas", data[19]);
+                    komandaIrasymas.Parameters.AddWithValue("@Det9Kiekis", data[20]);
+
+                    sqlPrisijungimas.Open();
+                    int rezult = komandaIrasymas.ExecuteNonQuery();
+                    if (rezult < 0)
+                    {
+                        Console.WriteLine("Error inserting data into Database!");
+                    }
+                    sqlPrisijungimas.Close();
+                }
+            }
+            MessageBox.Show("Komplektu sąrašas įrašytas į K5 duomenu bazę.");
         }
     }
 }
